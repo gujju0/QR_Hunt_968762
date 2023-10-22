c90fa082abe582302b18c66252e3e6ae45ab0021
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:36:36 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,83 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "How is stack found ?",
+    answer: "Research",
+    options: [
+      "AV002-B",
+      "AV113-C",
+      "AV102-D",
+      "AV012-A"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS used for?",
+    answer: "Robot",
+    options: [
+      "AV001-A",
+      "AV013-C",
+      "AV010-B",
+      "AV113-D"
+    ]
+  },
+  {
+    numb: 3,
+    question: "used for Eating (FAMOUS)?",
+    answer: "Dabeli",
+    options: [
+      "AV009-A",
+      "AV112-A",
+      "AV003-A",
+      "AV106-D"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does SQL used for?",
+    answer: "Device",
+    options: [
+      "AV006-B",
+      "AV107-D",
+      "AV014-C",
+      "AV111-A"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does XML used for?",
+    answer: "Pointer",
+    options: [
+      "AV113-D",
+      "AV003-C",
+      "AV012-C",
+      "AV102-C"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Height Of tree depends on ?",
+    answer: "Program",
+    options: [
+      "AV110-B",
+      "AV105-C",
+      "AV113-A",
+      "AV010-D"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Which parameter used to check first for softwere development cycle?",
+    answer: "Quality",
+    options: [
+      "AV114-D",
+      "AV104-D",
+      "AV013-B",
+      "AV001-C"
+    ]
+  },
+  
+];
